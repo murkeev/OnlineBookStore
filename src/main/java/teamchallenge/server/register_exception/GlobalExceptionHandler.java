6a5e83345e7a09ad596246712d0b3bcd23499450
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-07-06 16:42:22 +0300

    added package for register_exception, small refactor code.

--- src/main/java/teamchallenge/server/register_exception/GlobalExceptionHandler.java
+++ src/main/java/teamchallenge/server/register_exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package teamchallenge.server.exception;
+package teamchallenge.server.register_exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +7,9 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import teamchallenge.server.book.BookNotFoundException;
+import teamchallenge.server.image.FileUploadException;
+import teamchallenge.server.user.UserNotFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -44,6 +47,4 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleFileUploadException(FileUploadException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
-
-
 }

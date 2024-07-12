44bee2386b64e7b174e298e13633e5f206d88c1f
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-07-12 23:26:28 +0300

    added 'JpaSpecification' for BookRepository

--- src/main/java/teamchallenge/server/book/BookRepository.java
+++ src/main/java/teamchallenge/server/book/BookRepository.java
@@ -3,9 +3,10 @@ package teamchallenge.server.book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
     Page<Book> findAllByCategories(Pageable pageable, Category category);
 }
